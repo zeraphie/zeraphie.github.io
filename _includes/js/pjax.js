d5927ff9cc8387cb06d4dc4603224ea0fd5cc014
Tony Lopez <lopeax@users.noreply.github.com>
2017-03-09 12:09:09 +0000

    Create pjax.js

---
+++ _includes/js/pjax.js
@@ -0,0 +1,46 @@
+var fullyLoaded = function(callback){
+    window.onload = callback;
+    setupPjax(callback);
+};
+
+var pjax = function(url, callback, selector) {
+    var container = selector || '.body';
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+    xhr.responseType = 'document';
+
+    xhr.onload = function() {
+        find('title').textContent = find('title', this.response).textContent;
+
+        var newPage = find(container, this.response);
+        var currentPage = find(container);
+        currentPage.parentNode.replaceChild(newPage, currentPage);
+    };
+
+    xhr.send();
+
+    if(typeof callback === 'function'){
+        callback();
+    }
+};
+
+var setupPjax = function(callback){
+    if (history && history.pushState) {
+        var pjaxLinks = document.querySelectorAll('.pjax-link');
+        pjaxLinks.forEach(function(pjaxLink){
+            pjaxLink.addEventListener('click', function(e){
+                e.preventDefault();
+                pjax(e.target.href, callback);
+                history.pushState(null, null, e.target.href);
+            });
+        });
+
+        setTimeout(function() {
+            window.onpopstate = function() {
+                pjax(window.location.href, callback);
+            };
+        }, 1000);
+    }
+};
