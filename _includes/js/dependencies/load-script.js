58b2a4f34157cfd8a127c4aa3b1918bd10989310
Tony Lopez <lopeax@users.noreply.github.com>
2017-03-15 15:12:16 +0000

    Update load-script.js

--- _includes/js/dependencies/load-script.js
+++ _includes/js/dependencies/load-script.js
@@ -1,5 +1,5 @@
 window.loadScript = function(url, callback){
-    var head = document.getElementsByTagName('head')[0];
+    var head = document.querySelector('head');
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
