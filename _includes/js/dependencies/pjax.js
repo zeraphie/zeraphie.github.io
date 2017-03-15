9e8245f68cee47b57dbc891d316dd01475e93851
Tony Lopez <lopeax@users.noreply.github.com>
2017-03-15 14:44:29 +0000

    Update pjax.js

--- _includes/js/dependencies/pjax.js
+++ _includes/js/dependencies/pjax.js
@@ -43,13 +43,22 @@ pjax.request = function(url) {
     xhr.responseType = 'document';
     
     xhr.addEventListener('load', function(e){
-        self.find('title').textContent = self.find('title', this.response).textContent;
-        self.find('meta[name="description"]').setAttribute('content', self.find('meta[name="description"]', this.response).getAttribute('content'));
+        var response = this.response;
+        
+        self.find('title').textContent = self.find('title', response).textContent;
+        self.find('meta[name="description"]').setAttribute('content', self.find('meta[name="description"]', response).getAttribute('content'));
 
-        var newPage = self.find(self.container, this.response);
+        var newPage = self.find(self.container, response);
         var currentPage = self.find(self.container);
         currentPage.parentNode.replaceChild(newPage, currentPage);
 
+        var scripts = response.querySelectorAll('script');
+        scripts.forEach(function(code){
+            var script = document.createElement('script');
+            script.text = code.textContent;
+            document.head.appendChild(script).parentNode.removeChild(script);
+        });
+        
         if(typeof self.afterLoad === 'function'){
             self.afterLoad();
         }
