a8e7e6fde5101fc4835007569f00f47a3906689b
Tony Lopez <lopeax@users.noreply.github.com>
2017-03-15 13:57:31 +0000

    Update main.js

--- _includes/ui-experiment/main.js
+++ _includes/ui-experiment/main.js
@@ -187,7 +187,6 @@
     });
 
     pjax.onload(function() {
-        console.log(Class, CustomCursor, OppositeTrack, Flickity)
         var CLICK = (navigator.userAgent.match(/iPad/i)) ? 'touchstart' : 'click';
 
         var tiles = wrapper.querySelector('.tiles');
