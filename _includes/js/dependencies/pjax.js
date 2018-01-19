1f8915f175b51b90fdb4b269e887e865c890e684
Izzy Skye <zeraphie@users.noreply.github.com>
2018-01-19 14:31:26 +0000

    Update pjax.js

--- _includes/js/dependencies/pjax.js
+++ _includes/js/dependencies/pjax.js
@@ -8,16 +8,18 @@ class PJAX {
         this.links = links;
         this.replace = replace;
         this.queue = [];
+        
+        this.linkEvent = this.linkEvent.bind(this);
     }
-    
+
     static get DEFAULT_CONTAINER(){
         return '.body';
     }
-    
+
     static get DEFAULT_LINKS(){
         return '.pjax-link';
     }
-    
+
     static get DEFAULT_REPLACE(){
         return {
             textContent: [
@@ -47,67 +49,91 @@ class PJAX {
             ]
         }
     }
-    
+
     setup(){
         if(history && history.pushState){
             this.addLinkEvent(document.querySelectorAll(this.links));
-            
+
             window.onpopstate = () => {
                 this.request(window.location.href);
             };
-            
+
             this.execQueue();
         }
-        
+
         return this;
     }
-    
+
     addLinkEvent(pjaxLinks){
         pjaxLinks.forEach(pjaxLink => {
-            pjaxLink.addEventListener('click', e => {
-                e.preventDefault();
-                this.request(e.target.href);
-                history.pushState(null, null, e.target.href);
-            });
+            pjaxLink.addEventListener('click', this.linkEvent, true);
         });
-        
+
         return this;
-    };
+    }
+
+    linkEvent(e){
+        let url = e.currentTarget.href;
+
+        if(this.constructor.validPjaxLink(url)){
+            e.stopPropagation();
+            e.preventDefault();
+
+            this.request(url);
+            history.pushState(null, null, url);
+
+            return false;
+        }
+    }
+
+    static validPjaxLink(url){
+        if(url.indexOf(window.location.origin) === -1){
+            return false;
+        }
+
+        let fileRegex = new RegExp(/^.*\.([a-z0-9]+)$/i);
+
+        if(fileRegex.test(url)){
+            return false;
+        }
+
+        return true;
+    }
 
     request(url){
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url);
         xhr.responseType = 'document';
-        
+
         xhr.addEventListener('load', e => {
             let response = e.currentTarget.response;
-            
+
             for(let i = 0, len = this.replace.textContent.length; i < len; i++){
                 let elements = document.querySelectorAll(
                     this.replace.textContent[i]
                 );
-                
+
                 elements.forEach((element, key) => {
                     let found = response.querySelectorAll(
                         this.replace.textContent[i]
                     );
-                    
+
                     if(found.length && found[key]){
                         element.textContent = found[key].textContent;
                     }
                 });
             }
-            
+
             for(let i = 0, len = this.replace.attribute.length; i < len; i++){
                 let elements = document.querySelectorAll(
                     this.replace.attribute[i].selector
                 );
-                
+
                 elements.forEach((element, key) => {
                     let found = response.querySelectorAll(
                         this.replace.attribute[i].selector
                     );
-                    
+
                     if(found.length && found[key]){
                         element.setAttribute(
                             this.replace.attribute[i].attribute,
@@ -118,41 +144,41 @@ class PJAX {
                     }
                 });
             }
-            
-            this.addLinkEvent(response.querySelectorAll(
-                `${this.container} ${this.links}`
-            ));
-            
+
             let newPage = response.querySelector(this.container);
-            
+
             let currentPage = document.querySelector(this.container);
             currentPage.parentNode.replaceChild(newPage, currentPage);
-            
-            let scripts = response.querySelectorAll(
+
+            this.addLinkEvent(document.querySelectorAll(
+                `${this.container} ${this.links}`
+            ));
+
+            let scripts = document.querySelectorAll(
                 `${this.container} script`
             );
-            
+
             scripts.forEach(code => {
                 let script = document.createElement('script');
                 script.text = code.textContent;
                 document.head.appendChild(script).parentNode.removeChild(script);
             });
-            
+
             if(typeof this.afterLoad === 'function'){
                 this.afterLoad();
             }
         });
-        
+
         xhr.send();
-        
+
         return this;
     }
-    
+
     execQueue(){
         if(!(this.queue instanceof Array) || !this.queue.length){
             return this;
         }
-        
+
         this.queue.forEach(func => {
             if(typeof func === 'function'){
                 try {
@@ -162,18 +188,18 @@ class PJAX {
                 }
             }
         });
-        
+
         return this;
     }
-    
+
     onload(callback){
         if(typeof callback === 'function'){
             this.queue.push(callback);
         }
-        
+
         window.onload = this.execQueue;
         this.afterLoad = this.execQueue;
-        
+
         return this;
     }
 }
